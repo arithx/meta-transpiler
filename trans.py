f99af60ce04634b501c70f8ba8865672832f9742
Stephen Lowrie <stephen.lowrie@gmail.com>
2019-07-03 14:10:26 -0500

    fixup! trans: add transpiler

--- trans.py
+++ trans.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python3
+
 from argparse import ArgumentParser, FileType
 from datetime import datetime
 import json
 import os
-import sys
 
 
 def ensureDup(inp, out, inp_key, out_key):
@@ -11,108 +12,115 @@ def ensureDup(inp, out, inp_key, out_key):
         return out
     if out.get(out_key) != inp.get(inp_key):
         print("Input Files do not appear to be for the same release")
-        sys.exit(1)
+        raise SystemExit(1)
     return out
 
 def url_builder(stream, version, arch, path):
-    return "https://builds.coreos.fedoraproject.org/prod/streams/{stream}/builds/{version}/{arch}/{path}".format(
-            stream=stream, version=version, arch=arch, path=path)
+    return f"https://fcos-builds.s3.amazonaws.com/prod/streams/{stream}/builds/{version}/{arch}/{path}"
 
 def get_extension(path, ext_count):
     return ".".join(path.split(".")[-ext_count:])
 
 
 parser = ArgumentParser()
-parser.add_argument("file", type=FileType('r'), nargs='+')
-parser.add_argument("--output-file", help="Existing release.json to modify", required=False, default="release.json")
+parser.add_argument("--workdir", help="cosa workdir", required=True)
+parser.add_argument("--build-id", help="build id", required=True)
 args = parser.parse_args()
 
+outer_dir = os.path.join(args.workdir, "builds", args.build_id)
+release_file = os.path.join(outer_dir, "release.json")
+
 out = {}
-if args.output_file is not None:
-    if os.path.exists(args.output_file):
-        with open(args.output_file, 'r') as w:
-            out = json.load(w)
+if os.path.exists(release_file):
+    with open(release_file, 'r') as w:
+        out = json.load(w)
 
-for w in args.file:
-    input_ = json.load(w)
+build_dirs = [os.path.join(outer_dir, arch_dir) for arch_dir in os.listdir(outer_dir) if os.path.isdir(os.path.join(outer_dir, arch_dir))]
+files = [os.path.join(build_dir, "meta.json") for build_dir in build_dirs if os.path.exists(os.path.join(build_dir, "meta.json"))]
 
-    # the arch is only present in the ref and in the pkgdiff :(
-    arch = input_.get("ref").split("/")[1]
+for f in files:
+    with open(f, 'r') as w:
+        input_ = json.load(w)
 
-    out = ensureDup(input_, out, "buildid", "release")
-    out = ensureDup(input_.get('coreos-assembler.container-config-git'), out, 'branch', 'stream')
+        arch = input_.get("coreos-assembler.basearch")
 
-    # build the architectures dict
-    arch_dict = {"media": {}}
-    arch_dict = ensureDup(input_, arch_dict, "ostree-commit", "commit")
-    generic_arches = [("qemu", 2), ("metal", 2), ("openstack", 2), ("vmware", 1)]
-    for ga, ext_count in generic_arches:
-        if input_.get("images", {}).get(ga, None) is not None:
-            i = input_.get("images").get(ga)
-            ext = get_extension(i.get('path'), ext_count)
-            arch_dict['media'][ga] = {
-                "artifacts": {
-                    ext: {
-                        "disk": {
-                            "location": url_builder(out.get('stream'), out.get('release'), arch, i.get('path')),
-                            "signature": "{}.sig".format(url_builder(out.get('stream'), out.get('release'), arch, i.get('path'))),
-                            "sha256": i.get("sha256")
+        out = ensureDup(input_, out, "buildid", "release")
+        out = ensureDup(input_.get('coreos-assembler.container-config-git'), out, 'branch', 'stream')
+
+        # build the architectures dict
+        arch_dict = {"media": {}}
+        arch_dict = ensureDup(input_, arch_dict, "ostree-commit", "commit")
+        generic_arches = [("aws", 2), ("qemu", 2), ("metal", 2), ("openstack", 2), ("vmware", 1)]
+        for ga, ext_count in generic_arches:
+            if input_.get("images", {}).get(ga, None) is not None:
+                i = input_.get("images").get(ga)
+                ext = get_extension(i.get('path'), ext_count)
+                arch_dict['media'][ga] = {
+                    "artifacts": {
+                        ext: {
+                            "disk": {
+                                "location": url_builder(out.get('stream'), out.get('release'), arch, i.get('path')),
+                                "signature": "{}.sig".format(url_builder(out.get('stream'), out.get('release'), arch, i.get('path'))),
+                                "sha256": i.get("sha256")
+                            }
                         }
                     }
                 }
-            }
 
-    # metal specific additions
-    arch_dict["media"]["metal"] = arch_dict["media"]["metal"] or {}
-    arch_dict["media"]["metal"]["artifacts"] = arch_dict["media"]["metal"]["artifacts"] or {}
-    if input_.get("images", {}).get("iso", None) is not None:
-        i = input_.get("images").get("iso")
-        arch_dict["media"]["metal"]["artifacts"]["installer.iso"] = {
-            "disk": {
+        # AMI specific additions
+        if input_.get("amis", None) is not None:
+            arch_dict["media"]["aws"] = arch_dict["media"].get("aws", {})
+            arch_dict["media"]["aws"]["images"] = arch_dict["media"]["aws"].get("images", {})
+            for ami_dict in input_.get("amis"):
+                arch_dict["media"]["aws"]["images"][ami_dict["name"]] = {
+                    "image": ami_dict["hvm"]
+                }
+
+        # metal specific additions
+        arch_dict["media"]["metal"] = arch_dict["media"].get("metal", {})
+        arch_dict["media"]["metal"]["artifacts"] = arch_dict["media"]["metal"].get("artifacts", {})
+        if input_.get("images", {}).get("iso", None) is not None:
+            i = input_.get("images").get("iso")
+            arch_dict["media"]["metal"]["artifacts"]["installer.iso"] = {
+                "disk": {
+                    "location": url_builder(out.get('stream'), out.get('release'), arch, i.get('path')),
+                    "signature": "{}.sig".format(url_builder(out.get('stream'), out.get('release'), arch, i.get('path'))),
+                    "sha256": i.get("sha256")
+                }
+            }
+        if input_.get("images", {}).get("kernel", None) is not None:
+            i = input_.get("images").get("kernel")
+            arch_dict["media"]["metal"]["artifacts"]["installer-pxe"] = arch_dict["media"]["metal"]["artifacts"].get("installer-pxe",{})
+            arch_dict["media"]["metal"]["artifacts"]["installer-pxe"]["kernel"] = {
                 "location": url_builder(out.get('stream'), out.get('release'), arch, i.get('path')),
                 "signature": "{}.sig".format(url_builder(out.get('stream'), out.get('release'), arch, i.get('path'))),
                 "sha256": i.get("sha256")
             }
-        }
-    if input_.get("images", {}).get("kernel", None) is not None:
-        i = input_.get("images").get("kernel")
-        arch_dict["media"]["metal"]["artifacts"]["installer-pxe"] = arch_dict["media"]["metal"]["artifacts"].get("installer-pxe",{})
-        arch_dict["media"]["metal"]["artifacts"]["installer-pxe"]["kernel"] = {
-            "location": url_builder(out.get('stream'), out.get('release'), arch, i.get('path')),
-            "signature": "{}.sig".format(url_builder(out.get('stream'), out.get('release'), arch, i.get('path'))),
-            "sha256": i.get("sha256")
-        }
-    if input_.get("images", {}).get("initramfs", None) is not None:
-        i = input_.get("images").get("initramfs")
-        arch_dict["media"]["metal"]["artifacts"]["installer-pxe"] = arch_dict["media"]["metal"]["artifacts"].get("installer-pxe", {})
-        arch_dict["media"]["metal"]["artifacts"]["installer-pxe"]["initramfs"] = {
-            "location": url_builder(out.get('stream'), out.get('release'), arch, i.get('path')),
-            "signature": "{}.sig".format(url_builder(out.get('stream'), out.get('release'), arch, i.get('path'))),
-            "sha256": i.get("sha256")
-        }
+        if input_.get("images", {}).get("initramfs", None) is not None:
+            i = input_.get("images").get("initramfs")
+            arch_dict["media"]["metal"]["artifacts"]["installer-pxe"] = arch_dict["media"]["metal"]["artifacts"].get("installer-pxe", {})
+            arch_dict["media"]["metal"]["artifacts"]["installer-pxe"]["initramfs"] = {
+                "location": url_builder(out.get('stream'), out.get('release'), arch, i.get('path')),
+                "signature": "{}.sig".format(url_builder(out.get('stream'), out.get('release'), arch, i.get('path'))),
+                "sha256": i.get("sha256")
+            }
 
+        # if architectures as a whole or the individual arch is empty just push our changes
+        if out.get('architectures', None) is None or out['architectures'].get(arch, None) is None:
+            oa = out.get('architectures', {})
+            oa[arch] = arch_dict
+            out['architectures'] = oa
+        # else check media warning if key present, appending if not
+        else:
+            out_arch = out['architectures'][arch]
+            for media_type, val in arch_dict.get('media').items():
+                if media_type not in out_arch['media']:
+                    out['architectures'][arch]['media'].update({media_type: val})
+                elif val == out_arch['media'][media_type]:
+                    continue
+                else:
+                    print("differing media type detected: input_file '{}', media_type '{}'".format(input_file, media_type))
+                    raise SystemExit(1)
 
-
-    # if architectures as a whole or the individual arch is empty just push our changes
-    if out.get('architectures', None) is None or out['architectures'].get(arch, None) is None:
-        oa = out.get('architectures', {})
-        oa[arch] = arch_dict
-        out['architectures'] = oa
-    # else check media warning if key present, appending if not
-    else:
-        out_arch = out['architectures'][arch]
-        for media_type, val in arch_dict.get('media').values():
-            if media_type not in out_arch['media']:
-                out['architectures'][arch]['media'].update({media_type: val})
-            elif val == out_arch['media'][media_type]:
-                continue
-            else:
-                print("differing media type detected: input_file '{}', media_type '{}'".format(input_file, media_type))
-                sys.exit(1)
-
-if args.output_file is not None:
-    with open(args.output_file, 'w') as w:
-        json.dump(out, w)
-else:
-    with open('release.json', 'w') as w:
-        json.dump(out, w)
+with open(release_file, 'w') as w:
+    json.dump(out, w)
